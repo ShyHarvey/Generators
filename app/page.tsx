effb0e55e5b5eb5d26c4fbc388847acac96a7d01
Ilya Andreichenko <ilia.andrejchenko@gmail.com>
2023-03-06 15:24:09 +0300

    add mouse paralax

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useRef, useLayoutEffect } from "react"
 import { gsap } from "gsap"
 import SplitType from 'split-type'
 import TextPlugin from "gsap/TextPlugin"
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
 
 
 export default function Home() {
@@ -34,38 +35,44 @@ export default function Home() {
 
   return (
     <main >
-      <div className="min-h-screen bg-matrix-black">
-        <div className="container  mx-auto p-4">
-          <div id="title" className='background-animate flex flex-row flex-wrap 
+      <MouseParallaxContainer globalFactorX={0.5} globalFactorY={0.5}>
+        <div className="min-h-screen bg-matrix-black">
+          <div className="container  mx-auto p-4">
+            <div id="title" className='background-animate flex flex-row flex-wrap
           text-3xl md:text-5xl xl:text-6xl max-w-full 
           font-bold bg-clip-text text-transparent font-russo text-shadow
           shadow-matrix-light bg-gradient-to-r from-yellow-300 via-red-700
           to-blue-600 
           ' >
-            Compliment generator
-          </div>
-          <div className="h-96">
-            <div ref={com} id="compliment" className='background-animate flex flex-row flex-wrap 
+              Compliment generator
+            </div>
+            <div className="h-96">
+              <div ref={com} id="compliment"
+                className='background-animate flex flex-row flex-wrap 
            mt-2 text-4xl md:text-7xl xl:text-8xl max-w-full 
           font-bold bg-clip-text text-transparent font-russo text-shadow
            shadow-matrix-light bg-gradient-to-r from-yellow-300 via-red-700
                to-blue-600 ' >
 
+              </div>
             </div>
-          </div>
-          <button
-            onClick={getCompliment}
-            className="mt-10 inline-block rounded px-6 py-3 font-mononoki  text-3xl text-matrix-black
+            <MouseParallaxChild className="flex justify-center" factorX={1} factorY={1}>
+              <button
+                onClick={getCompliment}
+                className="inline-block mt-10 rounded px-6 py-3 font-mononoki text-3xl text-matrix-black
             border-solid border-2 border-sky-500 hover:shadow-md hover:shadow-sky-400
               uppercase leading-normal shadow-md 
-              transition duration-150 ease-in-out bg-gradient-to-r
-               from-indigo-600 via-amber-600 to-lime-700
+              transition duration-150 ease-in-out 
+              bg-gradient-to-r from-indigo-600 via-amber-600 to-lime-700
                 hover:from-indigo-400 hover:via-amber-300 hover:to-lime-400"
-          >
-            get compliment
-          </button>
+              >
+                get compliment
+              </button>
+            </MouseParallaxChild>
+
+          </div>
         </div>
-      </div>
+      </MouseParallaxContainer>
     </main>
   )
 }
