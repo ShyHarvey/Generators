18086996f009c156c3f738793c14d36746ef27da
Ilya Andreichenko <ilia.andrejchenko@gmail.com>
2023-03-06 14:49:52 +0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Generators',
@@ -14,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
 
       <body>{children}</body>
+      <Analytics />
     </html>
   )
 }
